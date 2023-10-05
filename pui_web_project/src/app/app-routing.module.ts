ff429cc8d2c1b7cb0a617a8a66e4b3d09b4c5c38
jps179 <janpensel@gmx.de>
2023-10-05 17:38:34 +0200

    Created the initial components & configured the routing for the components

--- pui_web_project/src/app/app-routing.module.ts
+++ pui_web_project/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
+import { ArticleEditionComponent } from './article-edition/article-edition.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: ArticleListComponent},
+  {path:"details", component: ArticleDetailsComponent},
+  {path:"edit", component: ArticleEditionComponent},
+  {path:"login", component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pui_web_project/src/app/app.module.ts
+++ pui_web_project/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
+import { ArticleEditionComponent } from './article-edition/article-edition.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleDetailsComponent,
+    ArticleEditionComponent,
+    ArticleListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
